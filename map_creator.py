7b628b600e26c58e44c124529cf68485c5128d49
Bohdan Bryk <bohdan.bryk@ucu.edu.ua>
2021-02-24 13:46:54 +0200

    change gitignore and add a documentation

--- map_creator.py
+++ map_creator.py
@@ -1,3 +1,7 @@
+"""
+This module contains functions to generate a map of locations 
+of friends of specific account on Twitter using Twitter API 
+"""
 import requests
 from folium import Map , FeatureGroup , Marker , LayerControl ,Icon
 from geopy.geocoders import Nominatim
@@ -12,6 +16,9 @@ def index():
     return render_template('data.html')
 
 def get_json():
+    """
+    gets json file of list of friends using twitter API
+    """
     surl = "https://api.twitter.com/1.1/friends/list.json"
     bearer_token = request.form.get("bearer_token")
     screen_name = request.form.get("screen_name")
@@ -27,7 +34,10 @@ def get_json():
 
 
 
-def get_screen_name_and_location(data:dict):
+def get_screen_name_and_location(data:dict)->dict:
+    """
+    gets screen_name and location name
+    """
     users = data["users"]
     name_and_location ={}
     for user in users:
@@ -36,7 +46,10 @@ def get_screen_name_and_location(data:dict):
             name_and_location[user["screen_name"]] = user["location"]
     return  name_and_location
 
-def get_locations_coordinates(name_and_location:dict):
+def get_locations_coordinates(name_and_location:dict)->dict:
+    """
+    createsa dict with location coordinates with screen name as a key
+    """
     name_and_coordinates = {}
     geolocator = Nominatim(user_agent='maap') 
     for name in name_and_location:
@@ -50,6 +63,9 @@ def get_locations_coordinates(name_and_location:dict):
 
 
 def generate_map(name_and_coordinates:dict):
+    """
+    generates map
+    """
     mp = Map( zoom_start=15) 
     markers = FeatureGroup() 
     for screen_name in name_and_coordinates:
@@ -62,6 +78,9 @@ def generate_map(name_and_coordinates:dict):
     return mp._repr_html_()
 
 def main():
+    """
+    main function. Runs the program
+    """
     response_json = get_json()
     name_and_location = get_screen_name_and_location(response_json)
     name_and_coordinates = get_locations_coordinates(name_and_location)
